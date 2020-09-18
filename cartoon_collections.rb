4a9541646177496f548aaf19aa28ae59f029825a
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-18 21:49:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index {|index, dwarves| puts {#{index}. #{dwarves}} 
+  array.each_with_index {|dwarves,index| puts {#{index}. #{dwarves}} 
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
